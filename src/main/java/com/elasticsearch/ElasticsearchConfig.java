8e26ccab00137172df8e9cf6e4469c283cdc19ef
LSH <lsh2613@naver.com>
2024-11-24 21:45:23 +0900

    [docs] README 작성

--- src/main/java/com/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/com/elasticsearch/ElasticsearchConfig.java
@@ -1,22 +1,22 @@
-package com.elasticsearch;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@EnableElasticsearchRepositories(basePackages = "com.elasticsearch")
-@Configuration
-public class ElasticsearchConfig extends ElasticsearchConfiguration {
-
-    @Value("${spring.data.elasticsearch.url}")
-    private String url;
-
-    @Override
-    public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(url)
-                .build();
-    }
-}
+//package com.elasticsearch;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.elasticsearch.client.ClientConfiguration;
+//import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+//
+//@EnableElasticsearchRepositories(basePackages = "com.elasticsearch")
+//@Configuration
+//public class ElasticsearchConfig extends ElasticsearchConfiguration {
+//
+//    @Value("${spring.data.elasticsearch.url}")
+//    private String url;
+//
+//    @Override
+//    public ClientConfiguration clientConfiguration() {
+//        return ClientConfiguration.builder()
+//                .connectedTo(url)
+//                .build();
+//    }
+//}
